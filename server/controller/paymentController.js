7ac4eee1433d276eb226313404b280d7b634fb10
Yash Gupta <yashnitagartala8@gmail.com>
2023-10-27 19:07:53 +0530

    done payment integration

--- server/controller/paymentController.js
+++ server/controller/paymentController.js
@@ -8,23 +8,52 @@ export const payment = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-      line_items: req.body.product.map((item) => {
-        return {
+      line_items: [
+        {
           price_data: {
             currency: "inr",
             product_data: {
-              name: item.title,
+              name: req.body.title.shortTitle, // Corrected here
             },
-            unit_amount: item.price * 100,
+            unit_amount: req.body.price.cost * 100, // Assuming price is also in req.body
           },
-          quantity: item.quantity,
-        };
-      }),
+          quantity: req.body.quantity,
+        },
+      ],
       success_url: "http://localhost:3002/success",
       cancel_url: "http://localhost:3002/cancel",
     });
     res.json({ url: session.url });
   } catch (error) {
+    console.log(error.message);
     res.status(500).json({ error: error.message });
   }
 };
+
+// export const payment = async (req, res) => {
+//   try {
+//     console.log(req.body);
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       mode: "payment",
+//       line_items: req.body.product.map((item) => {
+//         return {
+//           price_data: {
+//             currency: "inr",
+//             product_data: {
+//               name: item.title.shortTitle,
+//             },
+//             unit_amount: item.price.cost * 100,
+//           },
+//           quantity: item.quantity,
+//         };
+//       }),
+//       success_url: "http://localhost:3002/success",
+//       cancel_url: "http://localhost:3002/cancel",
+//     });
+//     res.json({ url: session.url });
+//   } catch (error) {
+//     console.log(error.message);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
